25c19397679af0ac3a3fed87f2b1923f65f365d9
s117507 <s117507@ap.be>
2025-06-20 04:36:08 +0000

    updated

--- Juni/starter/database.ts
+++ Juni/starter/database.ts
@@ -1,9 +1,13 @@
 import { Collection, MongoClient, SortDirection } from "mongodb";
 import dotenv from "dotenv";
 import { Penguin, Researcher, Species } from "./types";
+import bcrypt from "bcrypt";
 dotenv.config();
 
 export const client = new MongoClient(process.env.CONNECTION_STRING || "mongodb://localhost:27017");
+export const ResearchersCollection : Collection<Researcher> = client.db("Mock").collection<Researcher>("researchers");
+export const speciesCollection : Collection <Species> = client.db("Mock").collection<Species>("speciesCollection");
+export const penguinsCollection : Collection <Penguin> = client.db("Mock").collection<Penguin>("penguinsCollection");
 
 export const SALT_ROUNDS = 10;
 
@@ -18,15 +22,15 @@ async function exit() {
 }
 
 export async function getAllResearchers(): Promise<Researcher[]> {
-    return [];
+    return await ResearchersCollection.find({}).toArray();
 }
 
 export async function getAllSpecies(): Promise<Species[]> {
-    return [];
+    return await speciesCollection.find({}).toArray();
 }
 
 export async function getSpeciesById(id: string): Promise<Species | null> {
-    return null;
+    return await speciesCollection.findOne({ id: Number(id) });
 }
 
 export async function updateResearcher(username: string, newPincode: string): Promise<void> {
@@ -38,11 +42,13 @@ export async function assignPenguinToResearcher(penguinId: number, researcherStr
 };
 
 export async function getAllPenguins(sortField: string, sortDirection: SortDirection, q: string): Promise<Penguin[]> {
-    return [];
+    const filter = q ? {nickname: { $regex: q, $options: "i"}} : {};
+    const sort: Record<string, SortDirection> = {[sortField]: sortDirection,} 
+    return await penguinsCollection.find(filter).sort(sort).toArray();
 }
 
 export async function getPenguinsBySpecies(id: number): Promise<Penguin[]> {
-    return [];
+    return await penguinsCollection.find({ species_id: id }).toArray();
 }
 
 export async function login(username: string, pincode: string): Promise<Researcher | null> {
@@ -50,15 +56,60 @@ export async function login(username: string, pincode: string): Promise<Research
 }
 
 async function seedResearchers(): Promise<void> {
-   
+       const researchers : Researcher[] = await getAllResearchers();
+    if (researchers.length == 0) {
+        console.log("Database is empty, loading users from API")
+        const response = await fetch("https://raw.githubusercontent.com/similonap/json/refs/heads/master/penguins/researchers.json");
+        const researchers : Researcher[] = await response.json();
+
+    for (const r of researchers) {
+        r.pincode = await bcrypt.hash(r.pincode, SALT_ROUNDS);
+    }
+
+
+        await ResearchersCollection.insertMany(researchers);
+    } else {
+        console.log("Database has researchers")
+    }
 }
 
 async function seedSpecies(): Promise<void> {
-   
+          const species : Species[] = await getAllSpecies();
+    if (species.length == 0) {
+        console.log("Database is empty, loading species from API")
+        const response = await fetch("https://raw.githubusercontent.com/similonap/json/refs/heads/master/penguins/species.json");
+        const species : Species[] = await response.json();
+
+
+        await speciesCollection.insertMany(species);
+        console.log(`Inserted ${species.length} species`)
+    } else {
+        console.log("Database has species")
+    }
 }
 
 async function seedPenguins(): Promise<void> {
-    
+        const penguins : Penguin[] = await penguinsCollection.find({}).toArray();
+    if (penguins.length == 0) {
+        console.log("Database is empty, loading penguins from API")
+        const response = await fetch("https://raw.githubusercontent.com/similonap/json/refs/heads/master/penguins/penguins.json");
+        const penguins : Penguin[] = await response.json();
+
+        const allSpecies = await getAllSpecies();
+
+        for (const penguin of penguins) {
+            const species = allSpecies.find(s => s.id === penguin.species_id);
+            if (species) {
+                penguin.species = species;
+            }
+        }
+
+
+        await penguinsCollection.insertMany(penguins);
+        console.log(`Inserted ${penguins.length} penguins`)
+    } else {
+        console.log("Database has penguins")
+    }
 }
 
 export async function seedDatabase() {
--- Juni/starter/routers/speciesRouter.ts
+++ Juni/starter/routers/speciesRouter.ts
@@ -1,10 +1,27 @@
 import { Router } from "express";
+import { getSpeciesById, getPenguinsBySpecies } from "../database";
+import { calculatePenguinAge } from "../utils";
+
 
 export function speciesRouter() {
     const router = Router();
 
     router.get("/:id", async(req, res) => {
-        res.render("species");
+        const id = req.params.id;
+        const species = await getSpeciesById(id);
+
+        if (!species) {
+            res.status(404).send("Species not found");
+            return;
+        }
+
+        const penguins = await getPenguinsBySpecies(Number(id));
+
+        res.render("species",{
+            species,
+            penguins,
+            calculatePenguinAge}
+        );
     });
 
     return router;
